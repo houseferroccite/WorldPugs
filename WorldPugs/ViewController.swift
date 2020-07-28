22e351415a3135e837d37a0be576b6fedfcc4ac8
Alexei Zimovets <first.man.officila@yandex.ru>
2020-07-28 14:15:50 +0500

    Initial Commit

---
+++ WorldPugs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldPugs
+//
+//  Created by Алексей Зимовец on 28.07.2020.
+//  Copyright © 2020 Алексей Зимовец. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
